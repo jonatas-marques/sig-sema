4d5bddcbe7ed334041558169d5d9605cd4967baa
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2023-02-26 04:26:18 +0000

    Tentativa de publicar mapa OL

---
+++ app.js
@@ -0,0 +1,8 @@
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/XYZ';
+
+function MapGenerator() {
+    
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
-//Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.css'
-//Style CSS
-import styles from '/components/HomeMap.module.css'
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from '../styles.module.css'
+
+//const mapElement = useRef()
 
 function Home() {
     return (
         <div>
             <div
-                //className={styles.header}
-                class="header col-md-12">
+                className={styles.header}
+                class="col-md-12 text-center">
                 <h1>Prefeitura Municipal de Barueri</h1>
             </div>
-            <div id="side_panel" class="col-md-3">
-                <h1 class="text-center">Prédios Públicos</h1>
+            <div 
+                className={styles.side_panel}
+                class="col-md-3">
+                <h2 class="text-center">Prédios Públicos</h2>
                 <button id="zoomToSEMA" class="form-control btn-primary">Secretaria de Recursos Naturais e Meio Ambiente</button>
             </div>
-            <div id="mapdiv" class="col-md-9"></div>
-            <div id="footer" class="col-md-12">
+            <div 
+                //ref={mapElement}
+                className={styles.mapdiv} class="col-md-9">
+            </div>
+            <div className={styles.footer} class="col-md-12">
                 <h4 id="map_coords" class="text-center">Latitude: -23.5 Longitude: -46.8 Zoom Level: 13</h4>
                 <h4 class="text-center"> &copy;2023 <a href="https://portal.barueri.sp.gov.br/">Prefeitura de Barueri</a> </h4>
             </div>
