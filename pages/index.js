d8c3719874bb1f26ef68248ba2bc7af9714d07ab
Jonatas Marques <marjonatas@gmail.com>
2023-03-04 07:43:23 -0300

    Melhoria no layout e margens

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,18 @@ import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
 //import './styles.module.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import { fromLonLat } from 'ol/proj';
 
-const barueriLatLng = [-23.504003341782116, -46.87443782973699]; //Coordenadas de Barueri
+const barueriPlace = [-46.87443782973699, -23.504003341782116]; //Coordenadas de Barueri Lat Lon invertidas por causa do sistema cartesiano do OpenLayers
+const barueriWebMercator = fromLonLat(barueriPlace); // Precisa de transformação das coordenadas
+const ZoomPlace = 12;
 
 export default class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          center: barueriLatLng,
-          zoom: 9,
+          center: barueriWebMercator,
+          zoom: ZoomPlace,
       };
     }
 
@@ -27,9 +30,8 @@ export default class App extends React.Component {
                 }),
             ],
             view: new View({
-                center: barueriLatLng,
-                zoom: 9
-                ,
+                center: barueriWebMercator,
+                zoom: ZoomPlace,
             }),
         });
     }
@@ -38,11 +40,12 @@ export default class App extends React.Component {
         return (
         <div>
             <div class="row">
-                <div class="col-md-8"><h3>Sig Web</h3></div>
-                <div class="col-md-4" style={{textAlign: "right"}}><h3>Usuário Nome Servidor</h3></div>
+                <div class="col-md-8"><h3>Menu Sig Web</h3></div>
+                <div class="col-md-4" style={{textAlign: "Right"}}><h3>Usuário Nome Servidor</h3></div>
 
             </div>
-            <div class="row" style={{height:'100vh',width:'100%'}} id="map-container" className="map-container" />
+            <div class="row" style={{height:'850px',width:'100vw'}} id="map-container" className="map-container" />
+            <div class="row" style={{position: "relative", height: "50px", marginTop: "50px", textAlign: "center"}} id="footer">@2023 Prefeitura de Cidade</div>
         </div>
         );
     }
