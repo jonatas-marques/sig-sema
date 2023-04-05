4a50679bce855a5a216d6ba54807880e59bd6c2b
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2023-04-05 00:03:23 +0000

    Minor changes

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -111,7 +111,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            SEMA
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
