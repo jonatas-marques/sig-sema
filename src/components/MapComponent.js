83c1f9fe0843bcc3fd9fc314719d88c89270cd24
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2023-04-07 14:29:31 +0000

    Geolocation function

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -2,14 +2,16 @@ import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import XYZ from "ol/source/XYZ";
-import { useEffect } from "react";
-import {defaults} from 'ol/control/defaults';
+import { useEffect, useState } from "react";
 import Container from '@mui/material/Container';
 import styles from '/styles/Map.module.css'
 
-const placeBarueri = [-46.88576749900006, -23.509130797617008];
+const defaultCenter = [-46.88576749900006, -23.509130797617008];
+const defaultZoom = 13;
+const userZoom = 18;
 
-export default function MapComponent() {
+export default function MapComponent({ location }) {
+  
   useEffect(() => {
     new Map({
       target: "map",
@@ -25,11 +27,15 @@ export default function MapComponent() {
       view: new View({
         projection: "EPSG:4326",
         extent: [-180, -90, 180, 90],
-        center: placeBarueri,
-        zoom: 13,
+        center: location
+          ? [location.longitude, location.latitude]
+          : defaultCenter,
+        zoom: location
+          ? userZoom
+          : defaultZoom,
       }),
     });
-  }, []);
+  }, [location]);
 
   return (
     <Container maxWidth="false">
